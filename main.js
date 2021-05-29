c3bae02f0c30e351af933d03cc484d96e7b61da1
Ousmane Barry <79618101+ousmanebarry@users.noreply.github.com>
2021-05-29 19:42:11 -0400

    added event listeners

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+let userScore = document.getElementById('user-score');
+let compScore = document.getElementById('computer-score');
+let actionMsg = document.getElementById('action-message');
+let resultMsg = document.getElementById('result-message');
+
+const paperMove = document.getElementById('p');
+const scissorsMove = document.getElementById('s');
+const rockMove = document.getElementById('r');
+
+paperMove.addEventListener('click', pMove);
+scissorsMove.addEventListener('click', sMove);
+rockMove.addEventListener('click', rMove);
+
+function pMove() {
+  console.log('Paper');
+}
+
+function sMove() {
+  console.log('Scissors');
+}
+
+function rMove() {
+  console.log('Rock');
+}
